3ffafee49bf77b2d770f92796f1d6038ba6bb34c
Shaikh Amir Rukmoddin <amirshaikh320340@gmail.com>
2026-04-14 18:39:02 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,18 +1,6 @@
-from http.server import BaseHTTPRequestHandler
-import json
-
-class handler(BaseHTTPRequestHandler):
-    def do_GET(self):
-        if self.path == "/api/test":
-            self.send_response(200)
-            self.send_header('Content-type', 'application/json')
-            self.end_headers()
-
-            response = {
-                "message": "Vercel Python API is working 🚀"
-            }
-
-            self.wfile.write(json.dumps(response).encode())
-        else:
-            self.send_response(404)
-            self.end_headers()
+def handler(request):
+    return {
+        "statusCode": 200,
+        "headers": {"Content-Type": "application/json"},
+        "body": '{"message": "Working 🚀"}'
+    }
